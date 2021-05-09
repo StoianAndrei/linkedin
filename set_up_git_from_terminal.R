05ee56641ff3640aa5183a8e3cc0ed4fe85a3e7b
Andrei Stoian <stoian.andrei.mircea@gmail.com>
2021-05-09 17:08:47 +1200

    push an existing repository from the command line

---
+++ set_up_git_from_terminal.R
@@ -0,0 +1,16 @@
+git config --global user.email "stoian.andrei.mircea@gmail.com"
+git config --global user.name "Andrei Stoian"
+# …or create a new repository on the command line
+# echo "# linkedin" >> README.md
+# git init
+# git add README.md
+# git commit -m "first commit"
+# git branch -M main
+# git remote add origin https://github.com/StoianAndrei/linkedin.git
+# git push -u origin main
+
+
+# …or push an existing repository from the command line
+# git remote add origin https://github.com/StoianAndrei/linkedin.git
+# git branch -M main
+# git push -u origin main
